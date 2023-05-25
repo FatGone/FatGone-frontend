38105ab4910046d02bde94d554acb18f86336b95
anaastastasiia <nastiaproturenko5@gmail.com>
2023-05-25 23:23:53 +0200

    added login page with vuetify tags

---
+++ src/common/components/BackgroundGirl.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import backgroundGirl from '@/assets/svg/backgroundGirl.svg'
+
+defineProps ({classes: String, width: Number})
+</script>
+
+<template>
+    <v-img :src="backgroundGirl" :width="width" :class="classes" alt="girl"></v-img> 
+</template>
+
+<style scoped></style>
