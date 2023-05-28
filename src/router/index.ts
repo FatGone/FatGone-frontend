5033177e9c6e5e0651648c191ef3910338ba7ae7
Daniel <daniel.oblak99@gmail.com>
2023-05-28 14:23:53 +0200

    Added new routes for onboarding

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/auth/pages/LoginPage.vue'
+import CodeStep from '@/onboarding/pages/CodeStep.vue'
+import PersonalDetailsStep from '@/onboarding/pages/PersonalDetailsStep.vue'
+import MembershipType from '@/onboarding/pages/MembershipType.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +11,21 @@ const router = createRouter({
       name: 'Login',
       component: LoginPage,
     },
+    {
+      path: '/onboarding/code',
+      name: 'OnboardingCode',
+      component: CodeStep,
+    },
+    {
+      path: '/onboarding/personal-details',
+      name: 'OnboardingPersonalDetails',
+      component: PersonalDetailsStep,
+    },
+    {
+      path: '/onboarding/membership-type',
+      name: 'OnboardingMembershipType',
+      component: MembershipType,
+    }
   ]
 })
 
