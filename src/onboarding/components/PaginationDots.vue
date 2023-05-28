2087652a3f0832544e5dbba49070758e4e091cd5
Daniel <daniel.oblak99@gmail.com>
2023-05-28 14:23:12 +0200

    PaginationDots implementation

---
+++ src/onboarding/components/PaginationDots.vue
@@ -0,0 +1,48 @@
+
+<script lang="ts" setup>
+import router from '@/router';
+
+
+
+
+defineProps({ step: { type: Number, required: true } })
+const routes = ['', '/onboarding/code', '/onboarding/personal-details', '/onboarding/membership-type'];
+
+function navigationIntent(step: number): void {
+    router.push(routes[--step])
+}
+
+</script>
+
+<template>
+    <v-row class="pt-16" justify="center">
+        <v-btn v-if="step != 1" @click="navigationIntent(step)" icon>
+            <v-icon class="pb-1">mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-sheet v-if="step == 1" color="secondary" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-else color="secondary-container" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-if="step == 2" color="secondary" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-else color="secondary-container" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-if="step == 3" color="secondary" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-else color="secondary-container" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-if="step == 4" color="secondary" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-else color="secondary-container" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-if="step == 5" color="secondary" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-else color="secondary-container" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-if="step == 6" color="secondary" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+        <v-sheet v-else color="secondary-container" max-width="16" class="rounded-circle mx-2 " height="16"
+            width="100%"></v-sheet>
+    </v-row>
+</template>
+
