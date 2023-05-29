ca59c9c774b2e7881f2451f3e81f86c69027ff21
Doic <michalakradek2@gmail.com>
2023-05-29 22:27:39 +0200

    added route to landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/auth/pages/LoginPage.vue'
+import LandingPage from '@/landing/pages/LandingPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: '',
+      component: LandingPage,
+    },
     {
       path: '/login',
       name: 'Login',
