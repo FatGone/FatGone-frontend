92db8665e6421a27ee923ac349e9ec9e5c970b3f
mosinsky <mosmich2001@gmail.com>
2023-05-30 22:46:15 +0200

    Implemented validation for Membership form, re-position components, code-formatting

--- src/onboarding/pages/MembershipType.vue
+++ src/onboarding/pages/MembershipType.vue
@@ -4,93 +4,120 @@ import BackgroundGirl from '@/common/components/BackgroundGirl.vue'
 import Logo from '@/common/components/FGLogo.vue'
 import PaginationDots from '@/onboarding/components/PaginationDots.vue'
 import router from '@/router';
+import { useOnboardingStore } from '../stores/OnboardingStore';
 
+const onboardingStore = useOnboardingStore();
 
-function navigationIntent(): void {
+function _updateMembershipType(typeId: number) {
+    onboardingStore.updateMembershipType(typeId);
+    _navigationIntent();
+}
 
+function _navigationIntent(): void {
     router.push('/onboarding/card');
 }
+
 
 </script>
 <template>
     <v-layout>
         <v-main>
             <v-container>
-                <Logo :width="567" />
-                <v-row>
-                    <v-col cols="9" align-self="center">
-                        <p class="pt-16 fg-display-medium text-on-background">Wybierz rodzaj karnetu</p>
-                        <v-container>
-                            <v-row>
-                                <v-col>
-                                    <v-card color="background" class="w-100 px-8 mt-8 rounded-lg">
-                                        <p class="fg-display-small text-on-surface pt-6">Karnet</p>
-                                        <p class="pt-3 text-primary fg-display-large-acetone">HALF-OPEN</p>
-                                        <v-row class="px-8 pt-6">
-                                            <v-icon class="pr-4" icon="mdi-check" color="primary"></v-icon>
-                                            <p class="text-on-surface fg-body-large">Dostęp do wszystkich obiektów FATGONE
-                                            </p>
-                                        </v-row>
-                                        <v-row class="px-8 pt-6">
-                                            <v-icon class="pr-4" icon="mdi-check" color="primary"></v-icon>
-                                            <p class="text-on-surface fg-body-large">Dodatkowo płatny dostęp do sekcji SPA i
-                                                Wellness</p>
-                                        </v-row>
-                                        <v-row class="px-8 pt-6">
-                                            <v-icon class="pr-4" icon="mdi-check" color="primary"></v-icon>
-                                            <p class="text-on-surface fg-body-large">4 zajęcia grupowe w miesiącu</p>
-                                        </v-row>
-                                        <v-row class="px-8 pt-6">
-                                            <v-icon class="pr-4" icon="mdi-check" color="primary"></v-icon>
-                                            <p class="text-on-surface fg-body-large">1 konsultacja z trenerem personalnym w
+                <v-row class="h-screen" align-content="center">
+                    <v-col class="d-flex flex-column justify-center align-center" cols="5" offset="2">
+                        <Logo :height="163" :width="567" />
+                        <p class="text-on-background fg-display-medium pt-16">Wybierz rodzaj karnetu</p>
+                        <v-row>
+                            <v-col>
+                                <v-card color="background" class="w-100 px-8 mt-8 pb-6 rounded-lg text-center">
+                                    <p class="fg-display-small text-on-surface pt-6">Karnet</p>
+                                    <p class="pt-3 text-primary fg-display-large-acetone">HALF-OPEN</p>
+                                    <v-row class="px-8 pt-6">
+                                        <p>
+                                            <v-icon class="pa-0" icon="mdi-check" color="primary"></v-icon>
+                                            <span class="pa-0 pl-2 text-on-surface fg-body-large">Dostęp do wszystkich
+                                                obiektów FATGONE
+                                            </span>>
+                                        </p>
+                                    </v-row>
+                                    <v-row class="px-8 pt-6">
+                                        <p>
+                                            <v-icon class="pa-0" icon="mdi-check" color="primary"></v-icon>
+                                            <span class="pa-0 pl-2 text-on-surface fg-body-large">Dodatkowo płatny dostęp do
+                                                sekcji
+                                                SPA i
+                                                Wellness</span>
+                                        </p>
+                                    </v-row>
+                                    <v-row class="px-8 pt-6">
+                                        <p>
+                                            <v-icon class="pa-0" icon="mdi-check" color="primary"></v-icon>
+                                            <span class="pa-0 pl-2 text-on-surface fg-body-large">4 zajęcia grupowe w
+                                                miesiącu</span>
+                                        </p>
+                                    </v-row>
+                                    <v-row class="px-8 pt-6">
+                                        <p>
+                                            <v-icon class="pa-0" icon="mdi-check" color="primary"></v-icon>
+                                            <span class="pa-0 pl-2 text-on-surface fg-body-large">1 konsultacja z trenerem
+                                                personalnym w
                                                 miesiącu
-                                            </p>
-                                        </v-row>
-                                        <p class="pt-6 fg-headline-medium">49 zł<span
-                                                class="pa-0 text-primary fg-headline-medium">/</span>msc</p>
-                                        <Button class="w-100 py-2 px-16 mb-8 " label="Wybieram"
-                                            @click="navigationIntent()"></Button>
+                                            </span>
+                                        </p>
+                                    </v-row>
+                                    <p class="pt-12 fg-headline-medium">49 zł<span
+                                            class="pa-0 text-primary fg-headline-medium">/</span>msc</p>
+                                    <Button class="w-100 py-2 px-16 mb-8 " label="Wybieram"
+                                        @click="_updateMembershipType(1)"></Button>
 
-                                    </v-card>
-                                </v-col>
-                                <v-col class="d-flex">
-                                    <v-card color="background"
-                                        class="align-center justify-center w-100 px-8 mt-8 rounded-lg">
-                                        <p class="fg-display-small text-on-surface pt-6">Karnet</p>
-                                        <p class="pt-3 text-primary fg-display-large-acetone">OPEN</p>
-                                        <v-row class="px-8 pt-6">
-                                            <v-icon class="pr-4" icon="mdi-check" color="primary"></v-icon>
-                                            <p class="text-on-surface fg-body-large">Dostęp do wszystkich obiektów FATGONE
-                                            </p>
-                                        </v-row>
-                                        <v-row class="px-8 pt-6">
-                                            <v-icon class="pr-4" icon="mdi-check" color="primary"></v-icon>
-                                            <p class="text-on-surface fg-body-large">Dostęp do sekcji SPA i Wellness</p>
-                                        </v-row>
-                                        <v-row class="px-8 pt-6">
-                                            <v-icon class="pr-4" icon="mdi-check" color="primary"></v-icon>
-                                            <p class="text-on-surface fg-body-large">Wszystkie zajęcia grupowe</p>
-                                        </v-row>
-                                        <v-row class="px-8 pt-6">
-                                            <v-icon class="pr-4" icon="mdi-check" color="primary"></v-icon>
-                                            <p class="text-on-surface fg-body-large">Trening przygotowany i
+                                </v-card>
+                            </v-col>
+                            <v-col class="d-flex">
+                                <v-card color="background" class="align-center justify-center w-100 px-8 mt-8 rounded-lg ">
+                                    <p class="fg-display-small text-on-surface pt-6 text-center">Karnet</p>
+                                    <p class="pt-3 text-primary fg-display-large-acetone text-center">OPEN</p>
+                                    <v-row class="px-8 pt-6">
+                                        <p>
+                                            <v-icon class="pa-0" icon="mdi-check" color="primary"></v-icon>
+                                            <span class="pa-0 pl-2 text-on-surface fg-body-large">Dostęp do wszystkich
+                                                obiektów FATGONE
+                                            </span>
+                                        </p>
+                                    </v-row>
+                                    <v-row class="px-8 pt-6">
+                                        <p>
+                                            <v-icon class="pa-0" icon="mdi-check" color="primary"></v-icon>
+                                            <span class="pa-0 pl-2 text-on-surface fg-body-large">Dostęp do sekcji SPA i
+                                                Wellness
+                                            </span>
+                                        </p>
+                                    </v-row>
+                                    <v-row class="px-8 pt-6">
+                                        <p>
+                                            <v-icon class="pa-0" icon="mdi-check" color="primary"></v-icon>
+                                            <span class="pa-0 pl-2 text-on-surface fg-body-large">Wszystkie zajęcia
+                                                grupowe</span>
+                                        </p>
+                                    </v-row>
+                                    <v-row class="px-8 pt-6">
+                                        <p>
+                                            <v-icon class="pa-0" icon="mdi-check" color="primary"></v-icon>
+                                            <span class="pa-0 pl-2 text-on-surface fg-body-large">Trening przygotowany i
                                                 nadzorowany przez naszego specjalistę dostosowany do ciebie
-                                            </p>
-                                        </v-row>
-                                        <p class="pt-6 fg-headline-medium">99 zł<span
-                                                class="pa-0 text-primary fg-headline-medium">/</span>msc</p>
-                                        <Button class="w-100 py-2 px-16 mb-8 " label="Wybieram"
-                                            @click="navigationIntent()"></Button>
+                                            </span>
+                                        </p>
+                                    </v-row>
+                                    <p class="pt-6 fg-headline-medium text-center">99 zł<span
+                                            class="pa-0 text-primary fg-headline-medium ">/</span>msc</p>
+                                    <Button class="w-100 py-2 px-16 mb-8 " label="Wybieram"
+                                        @click="_updateMembershipType(2)"></Button>
 
-                                    </v-card>
-                                </v-col>
-                            </v-row>
-                        </v-container>
+                                </v-card>
+                            </v-col>
+                        </v-row>
                         <PaginationDots :step=4 />
                     </v-col>
-                    <v-col cols="3">
-                        <BackgroundGirl />
-                    </v-col>
+                    <BackgroundGirl :width="750" style="position: absolute; right: 250px; bottom:0px" />
                 </v-row>
             </v-container>
         </v-main>
