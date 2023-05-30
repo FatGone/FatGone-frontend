973b1d92989a9b33eebeb52c25b505d0879b78b2
mosinsky <mosmich2001@gmail.com>
2023-05-30 21:07:30 +0200

    Added form validation, changed page layout

--- src/onboarding/pages/CodeStep.vue
+++ src/onboarding/pages/CodeStep.vue
@@ -4,10 +4,24 @@ import BackgroundGirl from '@/common/components/BackgroundGirl.vue'
 import Logo from '@/common/components/FGLogo.vue'
 import PaginationDots from '@/onboarding/components/PaginationDots.vue'
 import router from '@/router'
+import { useOnboardingStore } from '../stores/OnboardingStore'
+import { ref } from 'vue'
 
-function navigationIntent(): void {
+const onboardingStore = useOnboardingStore();
+const code = ref(onboardingStore.code);
 
+function _navigationIntent(): void {
   router.push('/onboarding/personal-details');
+}
+function updateCode(): void {
+  if (_isFormValid(code.value)) {
+    onboardingStore.updateCode(code.value);
+    _navigationIntent();
+  }
+}
+const _isFormValid = function (code: string): boolean {
+  const pattern = /^[0-9]*$/;
+  return !!code && code.length <= 6 && pattern.test(code)
 }
 
 </script>
@@ -15,29 +29,41 @@ function navigationIntent(): void {
   <v-layout>
     <v-main>
       <v-container>
-        <v-row>
-          <v-col cols="5" offset="2" align-self="center">
-            <Logo :width="567" />
-            <div class="d-flex flex-column align-center">
-              <p class="text-on-background fg-display-medium pt-16">Potwierdź swój adres e-mail</p>
-              <v-card color="background" class="w-100 mx-16 mt-8 rounded-lg">
-                <v-form class="px-8 pt-8">
-                  <v-label class=" pb-2 text-wrap text-center"
-                    text="Na twój adres e - mail kon**********@gmail.com wysłaliśmy wiadomość z kodem weryfikacyjnym podaj go w polu poniżej żeby dokończyć rejestrację">
-                  </v-label>
-                  <v-text-field variant="outlined" label="Kod weryfikacyjny"></v-text-field>
-                  <Button class="w-100 py-2" label="Potwierdź" @click="navigationIntent()"></Button>
-                </v-form>
-              </v-card>
-            </div>
-
+        <v-row class="h-screen" align-content="center">
+          <v-col class="d-flex flex-column justify-center align-center" cols="5" offset="2">
+            <Logo :height="163" :width="567" />
+            <p class="text-on-background fg-display-medium pt-16">Potwierdź swój adres e-mail</p>
+            <v-card color="background" class="w-100 mx-16 mt-8 rounded-lg">
+              <v-form class="px-8 pt-8" @submit.prevent>
+                <v-label class=" pb-2 text-wrap text-center fb-body-large"
+                  text="Na twój adres e - mail kon**********@gmail.com wysłaliśmy wiadomość z kodem weryfikacyjnym podaj go w polu poniżej żeby dokończyć rejestrację">
+                </v-label>
+                <v-text-field class="mb-2" :rules="[rules.required, rules.codeLength, rules.number]" clearable
+                  variant="outlined" label="Kod weryfikacyjny" v-model="code"></v-text-field>
+                <Button class="w-100 py-2" label="Potwierdź" @click="updateCode"></Button>
+              </v-form>
+            </v-card>
             <PaginationDots :step=2 />
           </v-col>
-          <v-col cols="5">
-            <BackgroundGirl />
-          </v-col>
+          <BackgroundGirl :width="750" style="position: absolute; right: 250px; bottom:0px" />
         </v-row>
       </v-container>
     </v-main>
   </v-layout>
 </template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    rules: {
+      required: (code: string) =>
+        !!code || 'Pole jest wymagane',
+      codeLength: (code: string) => !!code && code.length <= 6 || 'Kod weryfikacyjny składa się z 6 znaków',
+      number: function (code: string) {
+        const pattern = /^[0-9]*$/;
+        return pattern.test(code) || 'Niepoprawny format';
+      }
+    },
+  }),
+}
+</script>
