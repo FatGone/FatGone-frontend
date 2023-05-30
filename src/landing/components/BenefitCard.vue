bfea5c2c7c19057ff1744ad059358092babc5589
Daniel <mosmich2001@gmail.com>
2023-05-31 01:01:11 +0200

    Removed margin and padding in BenefitCard

--- src/landing/components/BenefitCard.vue
+++ src/landing/components/BenefitCard.vue
@@ -3,12 +3,12 @@ defineProps({ image: String, width: Number, height: Number, title: String, descr
 </script>
 
 <template>
-    <v-card class="mx-auto pa-8 ma-2 pa-2 h-100">
-        <div class="d-flex justify-center pt-8 align-center" style="height: 113px;">
+    <v-card>
+        <div class="d-flex justify-center pt-8 align-center" style="height: 213px;">
             <v-img contain :src="image" :width="width" :height="height"></v-img>
         </div>
-        <v-card-title class="fg-headline-small on-surface text-center pt-7 pb-8">{{ title }}</v-card-title>
-        <v-card-text class="fg-body-large on-surface text-justify pb-5 mx-16">
+        <v-card-title class="fg-headline-medium on-surface text-center pa-0 pt-7 pb-8">{{ title }}</v-card-title>
+        <v-card-text class="fg-title-large font-weight-regular on-surface text-justify pb-5 px-8" style="line-height: 1.5;">
             {{ description }}
         </v-card-text>
     </v-card>
