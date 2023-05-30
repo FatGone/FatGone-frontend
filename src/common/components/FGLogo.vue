c51adf511d73fa93c88882cd9b73510d84880ba2
mosinsky <mosmich2001@gmail.com>
2023-05-30 21:44:13 +0200

    Replaced v-img with img html tag in FGLogo component

--- src/common/components/FGLogo.vue
+++ src/common/components/FGLogo.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
 import logo from '@/assets/svg/fg-logo.svg'
 
-defineProps ({classes: String, width: Number})
+defineProps({ classes: String, width: Number, height: Number })
 </script>
 
 <template>
-    <v-img :src="logo" alt="logo" :width="width" :class="classes"></v-img>
+    <img :src='logo' alt="logo" :width="width" :class="classes" />
 </template>
 
 <style scoped></style>
