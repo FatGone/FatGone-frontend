d32ecc93a4056c04701e30521ed6475ee030595e
Daniel <mosmich2001@gmail.com>
2023-05-31 00:13:41 +0200

    Changed how BenefitCard is rendered, removed static width and height

--- src/landing/components/BenefitCard.vue
+++ src/landing/components/BenefitCard.vue
@@ -1,18 +1,15 @@
 <script lang="ts" setup>
-defineProps ({image: String, width: Number, height: Number, title: String, description: String})
+defineProps({ image: String, width: Number, height: Number, title: String, description: String })
 </script>
 
 <template>
-    <v-card class="mx-auto pa-8 ma-2 pa-2" width="360" height="501">
-        <div class="d-flex justify-center pt-8">
+    <v-card class="mx-auto pa-8 ma-2 pa-2 h-100">
+        <div class="d-flex justify-center pt-8 align-center" style="height: 113px;">
             <v-img contain :src="image" :width="width" :height="height"></v-img>
         </div>
-        <v-card-title class="fg-headline-small on-surface text-center pt-7 pb-8">{{title}}</v-card-title>
-        <v-card-text class="fg-body-large on-surface text-justify pb-5">
-            {{description}}
+        <v-card-title class="fg-headline-small on-surface text-center pt-7 pb-8">{{ title }}</v-card-title>
+        <v-card-text class="fg-body-large on-surface text-justify pb-5 mx-16">
+            {{ description }}
         </v-card-text>
     </v-card>
 </template>
-
-<style scoped>
-</style>
