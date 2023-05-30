021b608d9420f4d56b29acf58f62fcc73cc1f8bb
anaastastasiia <nastiaproturenko5@gmail.com>
2023-05-30 22:05:30 +0200

    validation for loginPage

--- src/auth/pages/LoginPage.vue
+++ src/auth/pages/LoginPage.vue
@@ -7,9 +7,15 @@ import router from '@/router'
 
 const email = ref('');
 const password = ref('');
+const formRef = ref<HTMLFormElement | null>(null);
 
-async function login() {
-    router.push('/');
+const required = (value: string) => !!value || "Pole jest wymagane";
+
+async function login(): Promise<void> {
+    const { valid } = formRef.value!.validate();
+    if(valid) {
+        router.push('/');
+    }
 }
 
 </script>
@@ -17,31 +23,27 @@ async function login() {
     <v-layout>
         <v-main>
             <v-container>
-                <v-row>
-                    <v-col cols="5" offset="2" align-self="center">
-                        <Logo :width="567" />
-                        <div class="d-flex flex-column align-center">
-                            <h1 class="text-on-background fg-display-medium pt-16">Logowanie</h1>
-                            <v-card color="background" class="w-100 mx-16 mt-8 rounded-lg">
-                              <v-form class="px-8 pt-8" @submit.prevent>
-                                <v-text-field variant="outlined" label="Adres e-mail" class="pr-2"></v-text-field>
-                                <v-text-field variant="outlined" label="Password" class="pr-2" type="password"></v-text-field>
-                                <div class="d-flex justify-end">
-                                    <v-btn class="pa-0 w-30 v-btn--none" variant="outlined">
-                                        <span class="pa-0 fg-label-large text-primary">Przypomnij hasło</span>
-                                    </v-btn>
-                                    <Button class="w-30 py-2" label="Zaloguj" @click="login"></Button>
-                                </div>
-                              </v-form>
-                            </v-card>
-                            <div class="mr-15 text-secondary mt-8">
-                                Nie masz jeszcze konta? <RouterLink to="/registration" class="text-primary">Zarejestruj się!</RouterLink>
+                <v-row class="h-screen" align-content="center">
+                    <v-col class="d-flex flex-column justify-center align-center" cols="5" offset="2">
+                        <Logo :height="163" :width="567" />
+                        <p class="text-on-background fg-display-medium pt-16">Logowanie</p>
+                        <v-card color="background" class="w-100 mx-16 mt-8 rounded-lg">
+                            <v-form ref="formRef" class="px-8 pt-8" @submit.prevent>
+                            <v-text-field v-model="email" variant="outlined" label="Adres e-mail" class="pr-2" :rules="[required]"></v-text-field>
+                            <v-text-field v-model="password" variant="outlined" label="Password" class="pr-2" type="password" :rules="[required]"></v-text-field>
+                            <div class="d-flex justify-end">
+                                <v-btn class="pa-0 w-30 v-btn--none" variant="outlined">
+                                    <span class="pa-0 fg-label-large text-primary">Przypomnij hasło</span>
+                                </v-btn>
+                                <Button class="w-30 py-2" label="Zaloguj" @click="login"></Button>
                             </div>
+                          </v-form>
+                        </v-card>
+                        <div class="mr-15 text-secondary mt-8">
+                            Nie masz jeszcze konta? <RouterLink to="/registration" class="text-primary">Zarejestruj się!</RouterLink>
                         </div>
                     </v-col>
-                    <v-col cols="5">
-                        <BackgroundGirl />
-                    </v-col>
+                    <BackgroundGirl :width="400" style="position: absolute; right: 250px; bottom:0px" />
                 </v-row>
             </v-container>
         </v-main>
