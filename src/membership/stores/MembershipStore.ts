6632c36b930f9c7c0f4781b367bd882702c33ae5
Daniel <daniel.oblak99@gmail.com>
2023-05-31 05:54:45 +0200

    MembershipStore implementation

---
+++ src/membership/stores/MembershipStore.ts
@@ -0,0 +1,23 @@
+import { defineStore } from "pinia";
+import { Membership } from "../models/Membership";
+
+interface IState {
+    memberShips: Array<Membership>,
+}
+export const useMembershipStore = defineStore('membership', {
+    state: (): IState => ({
+        memberShips: [
+            new Membership(1, 'HALF-OPEN', 49, false, 89.99),
+            new Membership(2, 'OPEN', 99, true, 59.99),
+        ],
+    }),
+    getters: {
+
+
+    },
+    actions: {
+        getMembershipById(id: number): Membership | undefined {
+            return this.memberShips.find(x => x.id == id);
+        }
+    }
+});
