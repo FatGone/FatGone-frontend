072d305fec48846218eb10e76a96d8f1ff76faaa
Daniel <daniel.oblak99@gmail.com>
2023-05-31 05:58:52 +0200

    Changed types in Membership model

--- src/membership/models/Membership.ts
+++ src/membership/models/Membership.ts
@@ -2,8 +2,8 @@ export class Membership {
     public constructor(
         public readonly id: number,
         public readonly name: string,
-        public readonly price: string,
-        public readonly wellnessAccess: string,
-        public readonly registrationFee: string,
+        public readonly price: number,
+        public readonly wellnessAccess: boolean,
+        public readonly registrationFee: number,
     ) { }
 }
